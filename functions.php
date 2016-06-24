01c780a11031d7857ef89ed9dd121885a55e6976
Daniel Dropik <ddropik@wisc.edu>
2016-06-24 14:51:13 -0500

    no-template-sidebar and styles

--- functions.php
+++ functions.php
@@ -443,4 +443,15 @@ function output_uw_wordpress_banner() {
 }
 endif;
 
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+global $post;
+if ( isset( $post ) ) {
+$classes[] = $post->post_type . '--' . $post->post_name;
+}
+return $classes;
+}
+
+add_filter( 'body_class', 'add_slug_body_class' );
+
 
